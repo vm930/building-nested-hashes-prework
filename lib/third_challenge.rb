6a1cea0635d18413a3c69879e780e0fa6b9303d0
abstract-animation-9914 <vm930@hotmail.com>
2019-01-15 20:31:17 +0000

    Done.

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,26 +3,55 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
+        :name => "Lady Montague",
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lady Montague",
+        :age =>"54"
         },
       :hero => {
         #your key/value pairs here
-        },
-      :hero_friends => []
+        :name =>"Romeo",
+        :age => "15"
+        
+      },
+      :hero_friends => [{
+        :name => "Benvolio",
+        :age => "17",
+        :attitude => "Worried"
+      },
+      {
+        :name =>"Mercutio",
+        :age ="18",
+        :attitude =>"hot-headed"
+      }]
    }, 
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        :name => "Lord Capulet",
+        :age =>"50"
         },
       :matriarch => {
+        :name =>"Lady Capulet",
+        :age =>"51"
         #your key/value pairs here
         },
       :heroine => {
+        :name => "Juliet",
+        :age => "15",
+        :attitude =>"alive"
         #your key/value pairs here
         },
-      :heroine_friends => []
+      :heroine_friends => [{
+        :name => "Steven",
+        :age =>"30",
+        :attitude =>"confused"
+      },{
+        :name => "Nurse",
+        :age => "44",
+        :attitude =>"worried"}]
    }
   }
 
